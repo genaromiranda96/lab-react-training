733a00b1caad57559151a0d920b147706a4e5878
“Genaro <ramoon1996@hotmail.com>
2021-06-09 13:30:44 +0200

    better code of the credit card

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
-
 const CreditCard = (props) => {
 
-    let typeCard = props.type;
     let { bgColor} = props
     let numberCard = props.number
-    let numberHide = numberCard.substring(12,16)
+    let numberHide = numberCard.substring(12,16) 
 
-    if (typeCard === 'Visa') {
-        return(
+    function checkCardImg() {
+        return props.type === "Visa" ? '/img/visa.png' : '/img/master-card.svg' 
+    }   
+    
+    return(
         <div className="CreditCard" style={{backgroundColor: `${bgColor}`, borderRadius: 10, color:`${props.color}` }}>
             <div className="CreditCard-img"> 
-                <img height='20px' src="./img/visa.png"/>
+                <img height='20px' src={checkCardImg(props.type)}/>
             </div>
         
             <div className="CreditCard-num"> 
@@ -28,27 +29,6 @@ const CreditCard = (props) => {
             </div>
         </div> 
         )   
-    } else if (typeCard === 'Master Card') {
-        return( 
-        <div className="CreditCard" style={{backgroundColor: `${bgColor}`, borderRadius: 10, color:`${props.color}` }}>
-            <div className="CreditCard-img"> 
-                <img height='20px' src="./img/master-card.svg"/>
-            </div>
-        
-            <div className="CreditCard-num"> 
-                <p><text className="CreditCard-dot-num" >.... .... .... </text>{numberHide}</p>
-            </div>
-        
-            <div className="display"> 
-                <p>Expires {props.expirationMonth}/{props.expirationYear}</p>
-                <p>{props.bank}</p>
-            </div>
-            <div >
-                <p className="text-l">{props.owner}</p>
-            </div>
-    </div> ) 
-    }
-
-}
+    } 
 
 export default CreditCard;
